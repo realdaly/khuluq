2a2a5d8ae6e1fbf58a31b10e049377db04018778
realdaly <mohammed96218@yahoo.com>
2023-05-06 17:41:57 +0300

    added gitignore file to ignore venv dir

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\realdalyPC\Documents\khuluq\venv
+set VIRTUAL_ENV=C:\Users\moham\Documents\khuluq\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
